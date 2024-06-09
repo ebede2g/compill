0bd2517a385193f013212b052ea5705e447a428c
michael_ubuntu <michalrenyorsar@gmail.com>
2024-06-09 11:33:51 +0300

    0th commit. (skeleton)

---
+++ main.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template, request, session
+from flask_socketio import SocketIO, emit
+
+
+app = Flask(__name__, template_folder='pages', static_folder='scripts')
+app.secret_key = 'magic_3'
+socketio = SocketIO(app)
+
+
+
+
+if __name__ == '__main__':
+    socketio.run(app, debug=True, host='0.0.0.0', port=2001)
+
